694474c52689725111b3621a98ba6f388781fd8b
yamane yusuke <yusukeyamane01@gmail.com>
2016-10-15 19:56:44 +0900

    enable to make group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,4 +2,24 @@ class GroupsController < ApplicationController
   def index
     @groups = Group.all
   end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to group_chats_path(@group), notice: 'グループの作成に成功しました'
+    else
+      flash[:alert] = 'グループの作成に失敗しました'
+      render :new
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:chat_group).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "groups#index"
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:show] do
+  resources :groups, only: [:show, :new, :create, :edit, :update] do
     resources :chats, only: [:index, :new, :create]
   end
 end
