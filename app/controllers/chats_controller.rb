a502d45861a2c091268a9be4451a83aac0859de2
yamane yusuke <yusukeyamane01@gmail.com>
2016-11-06 02:30:13 +0900

    jsonのリクエストが来た時は、csrfを無効化

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,4 +1,5 @@
 class ChatsController < ApplicationController
+  skip_before_action :verify_authenticity_token, if: :json_request?
 
   def index
     set_groups_and_chats
@@ -7,17 +8,26 @@ class ChatsController < ApplicationController
   end
 
   def create
-    user = User.find(1)
-    @chat = user.chats.new(chats_params)
-    if @chat.save
-      redirect_to group_chats_path(params[:group_id])
-    else
-      set_group_and_group_and_chats
-      render :index
+    user = User.find(current_user.id)
+    @chat = current_user.chats.new(chats_params)
+    respond_to do |format|
+      if @chat.save
+        format.html { redirect_to group_chats_path(params[:group_id]) }
+        format.json {
+          render json: { message: @chat.body, name: current_user.name }
+        }
+      else
+        set_group_and_group_and_chats
+        render :index
+      end
     end
   end
 
   private
+
+  def json_request?
+    request.format.json?
+  end
 
   def set_groups_and_chats
     @groups = Group.all
