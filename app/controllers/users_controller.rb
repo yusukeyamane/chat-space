aaac88f1751163efebe544c3200296776332e490
yamane yusuke <yusukeyamane01@gmail.com>
2016-11-07 15:12:52 +0900

    インクリメンタルサーチ用のアクションを作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.where('name LIKE ?', "%#{params[:name]}%")
+
+    respond_to do |format|
+      format.json { render json: @users }
+    end
+  end
+
   def edit
     @user = current_user
   end
